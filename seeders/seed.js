ddbe3596516c1c24eec26bb547a63b151c418675
Jesse Willig <jessewillig@outlook.com>
2021-04-20 20:46:26 -0700

    seeding

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
+console.log(process.env.MONGODB_URI);
 let mongoose = require("mongoose");
 let db = require("../models");
-
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/workout',
