035c6d8d58dcc5b61d9962f1dda2872bb112166c
Jesse Willig <jessewillig@outlook.com>
2021-04-20 18:14:00 -0700

    small syntax

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ router.use(require("./routes/api.js"));
 router.use(require("./routes/html.js"));
 
 router.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", this.address().port, router.settings.env);
 });
